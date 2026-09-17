029ba3b42309eaa5c68791ae9cf054a08826f382
SeanPiedmonte <60246902+SeanPiedmonte@users.noreply.github.com>
2026-09-17 21:14:35 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "server.h"
 
 int main(int argc, char **argv) {
-    file_desc skt = socket(AF_INET, SOCK_STREAM, 0);
-    if (skt == -1) {
+    LinuxSocket *skt = lin_create_skt(8080, 0);
+    if (skt == NULL) {
         perror("ERROR [main]: ");
     }
 
-    int bind_res = lin_bind(skt, 80, 0);
+    int bind_res = lin_bind(skt);
     if (bind_res == -1) {
         return 1;
     }
@@ -14,5 +14,15 @@ int main(int argc, char **argv) {
     int listen_res = lin_listen(skt, 0);
     if (listen_res == -1) {
         return 2;
+    }
+
+    int acc;
+    while (TRUE) {
+        acc = lin_accept(skt);
+        if (acc == -1) {
+            printf("Issue with socket\n");
+        }
+
+        /*Implement a function to handle HTTP requests*/
     }
 }
