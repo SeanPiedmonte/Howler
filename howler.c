3df63c480289ee0250be2b8a1f72fb9202f26f97
SeanPiedmonte <60246902+SeanPiedmonte@users.noreply.github.com>
2026-09-17 14:55:28 -0500

    Update howler.c

--- howler.c
+++ howler.c
@@ -1,5 +1,37 @@
 #include "server.h"
 
+int lin_recv_msg(LinuxSocket *skt, Message *msg, int flags) {
+    int size = recv(skt->fd, msg->buffer, msg->buf_len, flags);
+    if (size < 0) {
+        /* signals an error */
+        perror("ERROR [lin_recv_msg]: ");
+        return 0;
+    }
+
+    return size;
+}
+
+LinuxSocket *lin_create_skt(uint16_t port, uint32_t ip_addr) {
+    file_desc fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (res == -1) {
+        swictch(errno) {
+            default:
+                perror("ERROR [lin_create_skt]: ");
+        }
+    }
+
+    LinuxSocket *skt = malloc(sizeof(LinuxSocket));
+    if (skt == NULL) {
+        return NULL;
+    }
+
+    skt->fd = fd;
+    skt->port = port;
+    skt->ip_addr = ip_addr;
+
+    return skt;
+}
+
 int lin_bind(file_desc skt, uint16_t port, uint32_t ip_addr) {
     struct sockaddr_in addr;
 
