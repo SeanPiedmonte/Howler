90bd6c4485c1c2c5c63af1e667685d8109ebdd91
SeanPiedmonte <60246902+SeanPiedmonte@users.noreply.github.com>
2026-09-17 14:51:20 -0500

    Update howler.h

--- howler.h
+++ howler.h
@@ -11,10 +11,16 @@
 typedef int file_desc
 
 typedef struct LinuxSocket {
-    file_desc fc;
+    file_desc fd;
+    
     uint16_t  port;
     uint32_t  ip_addr;
 } LinuxSocket;
+
+typedef struct Message {
+    int buf_len;
+    void *buffer;
+} Message; 
 
 enum StatusCode {
     STATUSOK = 200,
@@ -25,5 +31,7 @@ void put();
 void del();
 void post();
 
+LinuxSocket *lin_create_skt(uint16_t port, uint32_t ip_addr);
 int lin_bind(file_desc skt, uint16_port, uint32_t ip_addr);
 int lin_listen(file_desc skt, uint32_t port);
+int lin_recv_msg(LinuxSocket *skt, Message *msg, int flags);
